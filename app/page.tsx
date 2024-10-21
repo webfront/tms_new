6886f1b02cd40fd812a1723278670766fe8e3ee2
webfront <webfrontss@gmail.com>
2024-10-21 14:31:04 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
-          AFD Transaction Management System
+            Transaction Management System
         </h1>
 
         <p className="mt-3 text-2xl">
